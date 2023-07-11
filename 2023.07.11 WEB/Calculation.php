fd9f232a426a7b3a3036f371e4a735c457e16571
GAS Dilhara <136427386+sanudilhara@users.noreply.github.com>
2023-07-11 19:01:38 +0530

    Add files via upload

---
+++ 2023.07.11 WEB/Calculation.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<title>Calculation</title>
+<body>
+<?php
+
+$x=6;
+$y=4;
+
+echo "Sum: ".($x + $y)."<br>";
+echo "Sub: ".($x - $y)."<br>";
+echo "Mul: ".($x * $y)."<br>";
+echo "Div: ".($x / $y)."<br>";
+
+?>
+</body>
+</html>
---
+++ 2023.07.11 WEB/Web.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<title>Calculation</title>
+<body>
+<?php
+
+for($i=5; $i<=15; $i++)
+{
+ echo $i." ";
+}
+
+?>
+</body>
+</html>
